cc5b8730db940d36ee2de51347b0623dfa27f32b
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-31 15:39:10 -0300

    testing for ls command

--- Shell_testing_betty_style.c
+++ Shell_testing_betty_style.c
@@ -12,14 +12,16 @@ void shell(void)
 	char str[200], str1[] = "/bin/ls", str2[] = "exit";
 	char current_path[PATH_MAX];
 	getcwd(current_path, sizeof(current_path));
+	char *envp[] = {"PATH=/bin", 0};
 	char *argv[] = {"/bin/ls", "-l", current_path, NULL};
+
 
 	printf ("$ ");
 	scanf("%s", str);
 	printf("\n");
 	printf("Current working dir: %s\n", current_path);
 	if (!strcmp(str, str1))
-		execve(argv[0], argv, NULL);
+		execve(argv[0], argv, envp);
 	else
 		if(strcmp(str, str2))
 			printf("unknown command\n");
