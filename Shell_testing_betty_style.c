46a8f5f758b4e5165ba54bc7162af5822ee7f7b7
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-31 12:10:58 -0300

    testing ls comand in shell

--- Shell_testing_betty_style.c
+++ Shell_testing_betty_style.c
@@ -8,14 +8,15 @@
 
 void shell(void)
 {
-	char str[200], str1[] = "ls", str2[] = "exit";
+	char str[200], str1[] = "/bin/ls", str2[] = "exit";
+	char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
 
 	printf ("$ ");
 	scanf("%s", str);
 	printf("\n");
 
 	if (!strcmp(str, str1))
-		printf("folder empty\n");
+		execve(argv[0], argv, NULL);
 	else
 		if(strcmp(str, str2))
 			printf("unknown command\n");
