b263535f42b789d3f17ab83b2855658562cb7e84
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-31 12:25:58 -0300

    add header .h to sub folder

---
+++ pre_work/header.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include<string.h>  
+
+#endif
