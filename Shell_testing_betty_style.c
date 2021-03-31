e4aa888cee17f2a35660508c543fa16d75576b2c
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-31 16:33:07 -0300

    changed to ugly while for some reason

--- Shell_testing_betty_style.c
+++ Shell_testing_betty_style.c
@@ -6,7 +6,7 @@
 *
 *Return: 1 in success
 */
-
+/*
 void shell(void)
 {
 	char str[200], str1[] = "ls", str2[] = "exit";
@@ -31,7 +31,32 @@ void shell(void)
 			exit (99);
 	shell();
 }
+*/
+void shell(void)
+{
+	char str[200], str1[] = "ls", str2[] = "exit";
+	char current_path[PATH_MAX];
+	getcwd(current_path, sizeof(current_path));
+	char *envp[] = {"PATH=/bin", 0};
+	char *argv[100] = {"/bin/ls", current_path, NULL};
 
+	while (1)
+	{
+		printf ("$ ");
+		scanf("%s", str);
+		printf("Current working dir: %s\n", current_path);
+		if (!strcmp(str, str1))
+		{
+			if (fork() == 0)
+				execve(argv[0], argv, NULL);	
+		}
+		else
+			if(strcmp(str, str2))
+				printf("unknown command\n");
+			else
+				exit (99);
+	}
+}
 int main(void)
 {
 	shell();
