c47c15d05b346701b163a3ea05d25d6a687b6de8
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-31 16:07:05 -0300

    added lenght to argv

--- Shell_testing_betty_style.c
+++ Shell_testing_betty_style.c
@@ -6,31 +6,27 @@
 *
 *Return: 1 in success
 */
+
 void shell(void)
 {
 	char str[200], str1[] = "ls", str2[] = "exit";
 	char current_path[PATH_MAX];
 	getcwd(current_path, sizeof(current_path));
 	char *envp[] = {"PATH=/bin", 0};
-	char *argv[99];
-	argv[99] = NULL;
-	argv[98] = current_path;
-	argv[97] = "/bin/ls";
-	int i = 97;
+	char *argv[100] = {"/bin/ls", current_path, NULL};
 
 
 	printf ("$ ");
 	scanf("%s", str);
 	printf("Current working dir: %s\n", current_path);
 	if (!strcmp(str, str1))
-		execve(argv[i], argv, NULL);
+		execve(argv[0], argv, NULL);
 	else
 		if(strcmp(str, str2))
 			printf("unknown command\n");
 		else
 			exit (99);
 	shell();
-
 }
 
 int main(void)
