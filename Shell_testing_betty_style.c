c511a79b4ea32c136534fafc6c9c88d3ae9bf857
DiegoSusviela <dieguitosus@hotmail.com>
2021-03-31 16:04:58 -0300

    testing array with parameters fixed

--- Shell_testing_betty_style.c
+++ Shell_testing_betty_style.c
@@ -6,28 +6,31 @@
 *
 *Return: 1 in success
 */
-
 void shell(void)
 {
 	char str[200], str1[] = "ls", str2[] = "exit";
 	char current_path[PATH_MAX];
 	getcwd(current_path, sizeof(current_path));
 	char *envp[] = {"PATH=/bin", 0};
-	char *argv[] = {"/bin/ls", "-l", "-a", "-t",current_path, NULL};
+	char *argv[100];
+	argv[99] = NULL;
+	argv[98] = current_path;
+	argv[97] = "/bin/ls";
+	int i = 97;
 
 
 	printf ("$ ");
 	scanf("%s", str);
 	printf("Current working dir: %s\n", current_path);
 	if (!strcmp(str, str1))
-		execve(argv[0], argv, NULL);
+		execve(argv[i], argv, NULL);
 	else
 		if(strcmp(str, str2))
 			printf("unknown command\n");
 		else
 			exit (99);
 	shell();
-	
+
 }
 
 int main(void)
