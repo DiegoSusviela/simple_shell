5a108dc1f5345cdee71db68da69f57fc838fee02
DiegoSusviela <dieguitosus@hotmail.com>
2021-04-02 14:09:49 -0300

    bnack to hardcoding

--- shell.c
+++ shell.c
@@ -62,7 +62,7 @@ int find_and_run_command(char *usr_input)
 	*/
 	struct stat stats;
 	char pathname[] = "/bin/";
-	char *argv[100] = {pathname, NULL};
+	char *argv[100] = {"/bin/", NULL};
 
 	strcat(pathname, usr_input);
 	if(!stat(pathname, &stats))
