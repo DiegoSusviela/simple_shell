0ad54153a3e524e0092db11bdaa31d676147d121
toshi-uy <s_borgia@hotmail.com>
2021-04-12 22:39:54 +0000

    testing _strncmp

--- shell.c
+++ shell.c
@@ -156,7 +156,7 @@ char *_getenv(const char *name)
 		for (j = 0; environ[i][j] != '='; j++)
 		{
 		}
-		if (!strncmp(environ[i], name, j))
+		if (!_strncmp(environ[i], name, j))
 		{
 			j++;
 			while(environ[i][j + cont])
