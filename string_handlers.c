0789b1d3b5b7e4cd3826cb930a9b215e8b11bba5
DiegoSusviela <dieguitosus@hotmail.com>
2021-04-14 17:18:09 -0300

    solkving un val

--- string_handlers.c
+++ string_handlers.c
@@ -11,16 +11,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
-
-	for (i = 0; dest[i] != '\0'; i++)
+	int i = 0, j;
+	if (dest)
+		for (i = 0; dest[i] != '\0'; i++);
+	if (src)
 	{
+		for (j = 0; src[j] != '\0'; j++)
+			dest[i + j] = src[j];
+		dest[i + j] = '\0';
 	}
-	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[i + j] = src[j];
-	}
-	dest[i + j] = '\0';
 	return (dest);
 }
 
