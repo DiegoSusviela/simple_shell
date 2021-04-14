21dc126bab456e8d397084799aa60b1c8d9cbea3
DiegoSusviela <dieguitosus@hotmail.com>
2021-04-14 12:44:25 -0300

    fixed putchar comment

--- aux_func_1.c
+++ aux_func_1.c
@@ -33,11 +33,13 @@ void _puts(char *str)
 	_putchar('\n');
 }
 
+/*
  * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
